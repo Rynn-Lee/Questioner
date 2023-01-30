316d29fc91dd396b50c34169ec392eb11557e26d
_RynnLee <20amigo04@gmail.com>
2023-01-30 17:43:07 +0600

    Hotfix #1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import glob from "glob";
+import path from "path";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: path.join(__dirname, "src"),
+  build: {
+    outDir: path.join(__dirname, "dist"),
+    rollupOptions: {
+      input: glob.sync(path.resolve(__dirname, "src", "*.html")),
+    },
+  },
 })
